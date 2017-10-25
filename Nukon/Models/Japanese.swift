0373c48abb9c30364f61e545725a0303fb84d676
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-10-25 13:56:46 -0700

    first commit

---
+++ Nukon/Models/Japanese.swift
@@ -0,0 +1,9 @@
+//
+//  Japanese.swift
+//  Nukon
+//
+//  Created by Kaichi Momose on 2017/10/25.
+//  Copyright © 2017 Kaichi Momose. All rights reserved.
+//
+
+import Foundation
---
+++ Nukon/Views/JapaneseCharactersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JapaneseCharactersTableViewCell.swift
+//  Nukon
+//
+//  Created by Kaichi Momose on 2017/10/25.
+//  Copyright © 2017 Kaichi Momose. All rights reserved.
+//
+
+import UIKit
+
+class JapaneseCharactersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
