a80415fc45d051d921a298828419371e839650b5
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-10-25 07:12:48 -0700

    Initial Commit

---
+++ Nukon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nukon
+//
+//  Created by Kaichi Momose on 2017/10/25.
+//  Copyright © 2017 Kaichi Momose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
