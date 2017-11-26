86d9dcc6178dcc97ef3fd39d4f4591658eaaeb81
e-bushi <chrislovemauldin@gmail.com>
2017-11-26 02:02:59 -0800

    Update UserProfile UI

--- Nukon/Controllers/UserProfileViewController.swift
+++ Nukon/Controllers/UserProfileViewController.swift
@@ -8,13 +8,122 @@
 
 import UIKit
 
-class UserProfileViewController: UIViewController {
+@IBDesignable class UserProfileViewController: UIViewController {
 
+    @IBOutlet var backSuper: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var leaderboardIcon: UIImageView!
+    @IBOutlet weak var updatedViewWithArc: Arc!
+    
+    let arclayer = CAShapeLayer()
+    
+    //create leaderBoardIcon programmatically
+    func create() {
+        leaderboardIcon.center = CGPoint(x: backSuper.frame.width/3.295,
+                                 y: backSuper.bounds.height/4)
+        let size = CGSize(width: 90.0, height: 90.0)
+        leaderboardIcon.sizeThatFits(size)
+    }
+    
+    
+    let gradientLayer = CAGradientLayer()
+    let iconGradient = CAGradientLayer()
+    let layer = CALayer()
+    let leaderLayer = CALayer()
+    let prof = UIImage(named: "profile-1")?.cgImage
+    let leaderIcon = UIImage(named: "leaderboard")?.cgImage
+    
+    func setUpGradient() {
+        gradientLayer.colors = [
+//            UIColor(red: 255/255, green: 75/255, blue: 52/255, alpha: 1).cgColor,
+            UIColor(red: 236/255, green: 75/255, blue: 164/255, alpha: 1).cgColor,
+            UIColor(red: 255/255, green: 120/255, blue: 52/255, alpha: 1).cgColor,
+            UIColor(red: 252/255, green: 252/255, blue: 103/255, alpha: 1).cgColor
+        ]
+        
+        gradientLayer.locations = [0.35, 0.75, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        
+        gradientLayer.frame = backSuper.frame
+        backSuper.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func setUpIconGradient() {
+        iconGradient.frame = leaderboardIcon.frame
+        iconGradient.contents = leaderIcon
+        iconGradient.contentsGravity = kCAGravityCenter
+        iconGradient.isGeometryFlipped = false
+        iconGradient.cornerRadius = iconGradient.frame.width/2
+//        iconGradient.shadowOpacity = 0.7
+//        iconGradient.shadowRadius = 2.0
+//        iconGradient.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        
+        iconGradient.colors = [
+//            UIColor(red: 255/255, green: 75/255, blue: 52/255, alpha: 1).cgColor,
+//            UIColor(red: 236/255, green: 75/255, blue: 164/255, alpha: 1).cgColor,
+            UIColor(red: 255/255, green: 120/255, blue: 52/255, alpha: 1).cgColor,
+            UIColor(red: 252/255, green: 252/255, blue: 103/255, alpha: 1).cgColor
+        ]
+        
+        iconGradient.locations = [0.35, 0.75, 1.0]
+        iconGradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        iconGradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        
+        iconGradient.frame = leaderboardIcon.frame
+        leaderboardIcon.layer.insertSublayer(iconGradient, at: 0)
+    }
+    
+    func degreesToRadians(_ degrees: Double) -> CGFloat {
+        return CGFloat(degrees * .pi / 180.0)
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpImage()
+//        setUpLeaderBoardIcon()
+        
+        //create leader board icon
+        create()
+        setUpIconGradient()
 
+        
+        
         // Do any additional setup after loading the view.
+        profileImage.layer.addSublayer(layer)
+        leaderboardIcon.layer.addSublayer(iconGradient)
+        
+        
+        
+        updatedViewWithArc.setUp()
+        updatedViewWithArc.configure()
+        view.addSubview(updatedViewWithArc)
+        setUpGradient()
+        //add shape object to view
+//        let arcView = Arc(origin: view.center)
+//        self.view.addSubview(arcView)
     }
+    
+    
+    func setUpImage() {
+        layer.frame = profileImage.bounds
+        layer.contents = prof
+        layer.contentsGravity = kCAGravityCenter
+        layer.isGeometryFlipped = false
+        layer.cornerRadius = layer.frame.width / 2
+//        layer.backgroundColor = UIColor(red: 236/255, green: 75/255, blue: 164/255, alpha: 0.90).cgColor
+        layer.backgroundColor = UIColor.white.cgColor
+//        layer.borderWidth = 1
+//        layer.borderColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.7
+        layer.shadowRadius = 2.0
+        layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -33,3 +142,6 @@ class UserProfileViewController: UIViewController {
     */
 
 }
+
+
+
