0ee03483a1fe5eb6cb325e4120b0520ddd42adfe
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2018-03-11 08:04:19 -0700

    pull

---
+++ Nukon/Custom View Classes/CustomTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  CustomTabBarController.swift
+//  Nukon
+//
+//  Created by Kaichi Momose on 2018/03/10.
+//  Copyright © 2018 Kaichi Momose. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    //MARK:
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
